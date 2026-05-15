4f6f07f93d96e6681dc50bb20a7dcc6dfa807ffe
pankajrock12 <pankajpandey.rock12@gmail.com>
2026-05-15 17:40:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,58 +1,38 @@
-ENVIRONMENTS = {
+AWS_CONFIGS = {
 
     "DEV": {
-
-        "credentials": {
-            "aws_access_key_id": "YOUR_KEY",
-            "aws_secret_access_key": "YOUR_SECRET",
-            "aws_session_token": "YOUR_TOKEN",
-            "region": "us-east-1"
-        },
-
-        "tables": [
-            "pabio-bill-lookup-table-dev"
-        ]
+        "aws_access_key_id": "YOUR_DEV_ACCESS_KEY",
+        "aws_secret_access_key": "YOUR_DEV_SECRET_KEY",
+        "aws_session_token": "YOUR_DEV_SESSION_TOKEN",
+        "region": "us-east-1"
     },
 
     "TEST": {
-
-        "credentials": {
-            "aws_access_key_id": "YOUR_KEY",
-            "aws_secret_access_key": "YOUR_SECRET",
-            "aws_session_token": "YOUR_TOKEN",
-            "region": "us-east-1"
-        },
-
-        "tables": [
-            "pabio-bill-lookup-table-test"
-        ]
+        "aws_access_key_id": "YOUR_TEST_ACCESS_KEY",
+        "aws_secret_access_key": "YOUR_TEST_SECRET_KEY",
+        "aws_session_token": "YOUR_TEST_SESSION_TOKEN",
+        "region": "us-east-1"
     },
 
     "MO": {
-
-        "credentials": {
-            "aws_access_key_id": "YOUR_KEY",
-            "aws_secret_access_key": "YOUR_SECRET",
-            "aws_session_token": "YOUR_TOKEN",
-            "region": "us-east-1"
-        },
-
-        "tables": [
-            "pabio-bill-lookup-table-mo"
-        ]
+        "aws_access_key_id": "YOUR_MO_ACCESS_KEY",
+        "aws_secret_access_key": "YOUR_MO_SECRET_KEY",
+        "aws_session_token": "YOUR_MO_SESSION_TOKEN",
+        "region": "us-east-1"
     },
 
     "PROD": {
-
-        "credentials": {
-            "aws_access_key_id": "YOUR_KEY",
-            "aws_secret_access_key": "YOUR_SECRET",
-            "aws_session_token": "YOUR_TOKEN",
-            "region": "us-east-1"
-        },
-
-        "tables": [
-            "pabio-bill-lookup-table-prod"
-        ]
+        "aws_access_key_id": "YOUR_PROD_ACCESS_KEY",
+        "aws_secret_access_key": "YOUR_PROD_SECRET_KEY",
+        "aws_session_token": "YOUR_PROD_SESSION_TOKEN",
+        "region": "us-east-1"
     }
 }
+
+
+TABLES = [
+    "pabio-bill-lookup-table-dev",
+    "pabio-bill-lookup-table-test",
+    "pabio-bill-lookup-table-mo",
+    "pabio-bill-lookup-table-prod"
+]
