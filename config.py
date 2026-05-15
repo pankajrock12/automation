54b325c7fe1e10d84a3e8c31587854da0b1c5e4f
pankajrock12 <pankajpandey.rock12@gmail.com>
2026-05-15 17:54:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,38 +1,46 @@
 AWS_CONFIGS = {
 
     "DEV": {
-        "aws_access_key_id": "YOUR_DEV_ACCESS_KEY",
-        "aws_secret_access_key": "YOUR_DEV_SECRET_KEY",
-        "aws_session_token": "YOUR_DEV_SESSION_TOKEN",
-        "region": "us-east-1"
+        "region": "us-east-1",
+        "access_key": "YOUR_DEV_ACCESS_KEY",
+        "secret_key": "YOUR_DEV_SECRET_KEY"
     },
 
     "TEST": {
-        "aws_access_key_id": "YOUR_TEST_ACCESS_KEY",
-        "aws_secret_access_key": "YOUR_TEST_SECRET_KEY",
-        "aws_session_token": "YOUR_TEST_SESSION_TOKEN",
-        "region": "us-east-1"
+        "region": "us-east-1",
+        "access_key": "YOUR_TEST_ACCESS_KEY",
+        "secret_key": "YOUR_TEST_SECRET_KEY"
     },
 
     "MO": {
-        "aws_access_key_id": "YOUR_MO_ACCESS_KEY",
-        "aws_secret_access_key": "YOUR_MO_SECRET_KEY",
-        "aws_session_token": "YOUR_MO_SESSION_TOKEN",
-        "region": "us-east-1"
+        "region": "us-east-1",
+        "access_key": "YOUR_MO_ACCESS_KEY",
+        "secret_key": "YOUR_MO_SECRET_KEY"
     },
 
     "PROD": {
-        "aws_access_key_id": "YOUR_PROD_ACCESS_KEY",
-        "aws_secret_access_key": "YOUR_PROD_SECRET_KEY",
-        "aws_session_token": "YOUR_PROD_SESSION_TOKEN",
-        "region": "us-east-1"
+        "region": "us-east-1",
+        "access_key": "YOUR_PROD_ACCESS_KEY",
+        "secret_key": "YOUR_PROD_SECRET_KEY"
     }
 }
 
 
-TABLES = [
-    "pabio-bill-lookup-table-dev",
-    "pabio-bill-lookup-table-test",
-    "pabio-bill-lookup-table-mo",
-    "pabio-bill-lookup-table-prod"
-]
+TABLES = {
+
+    "DEV": [
+        "pabio-bill-lookup-table-dev"
+    ],
+
+    "TEST": [
+        "pabio-bill-lookup-table-test"
+    ],
+
+    "MO": [
+        "pabio-bill-lookup-table-mo"
+    ],
+
+    "PROD": [
+        "pabio-bill-lookup-table-prod"
+    ]
+}
