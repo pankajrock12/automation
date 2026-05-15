fb5cf14ee0bfe4a827970aca5ac09b8d83cbdfad
pankajrock12 <pankajpandey.rock12@gmail.com>
2026-05-15 17:27:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,58 @@
+ENVIRONMENTS = {
+
+    "DEV": {
+
+        "credentials": {
+            "aws_access_key_id": "YOUR_KEY",
+            "aws_secret_access_key": "YOUR_SECRET",
+            "aws_session_token": "YOUR_TOKEN",
+            "region": "us-east-1"
+        },
+
+        "tables": [
+            "pabio-bill-lookup-table-dev"
+        ]
+    },
+
+    "TEST": {
+
+        "credentials": {
+            "aws_access_key_id": "YOUR_KEY",
+            "aws_secret_access_key": "YOUR_SECRET",
+            "aws_session_token": "YOUR_TOKEN",
+            "region": "us-east-1"
+        },
+
+        "tables": [
+            "pabio-bill-lookup-table-test"
+        ]
+    },
+
+    "MO": {
+
+        "credentials": {
+            "aws_access_key_id": "YOUR_KEY",
+            "aws_secret_access_key": "YOUR_SECRET",
+            "aws_session_token": "YOUR_TOKEN",
+            "region": "us-east-1"
+        },
+
+        "tables": [
+            "pabio-bill-lookup-table-mo"
+        ]
+    },
+
+    "PROD": {
+
+        "credentials": {
+            "aws_access_key_id": "YOUR_KEY",
+            "aws_secret_access_key": "YOUR_SECRET",
+            "aws_session_token": "YOUR_TOKEN",
+            "region": "us-east-1"
+        },
+
+        "tables": [
+            "pabio-bill-lookup-table-prod"
+        ]
+    }
+}
