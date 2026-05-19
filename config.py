3bae5ae5ef79416002138270724098049fa772f0
pankajrock12 <pankajpandey.rock12@gmail.com>
2026-05-19 17:50:37 +0530

    Remove unnecessary blank lines in config.py

--- config.py
+++ config.py
@@ -30,12 +30,11 @@ AWS_CONFIGS = {
 
 }
 
-
 TABLES = {
-
     "DEV": "pablo-bill-lookup-table-dev",
     "TEST": "pablo-bill-lookup-table-test",
     "MO": "pablo-bill-lookup-table-mo",
     "PROD": "pablo-bill-lookup-table-prod"
 
 }
+
