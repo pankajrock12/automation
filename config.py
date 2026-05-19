f9009dce199e7348823ec2e91f69cb4c43aba550
pankajrock12 <pankajpandey.rock12@gmail.com>
2026-05-19 16:49:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,46 +1,10 @@
-AWS_CONFIGS = {
-
-    "DEV": {
-        "region": "us-east-1",
-        "access_key": "YOUR_DEV_ACCESS_KEY",
-        "secret_key": "YOUR_DEV_SECRET_KEY"
-    },
-
-    "TEST": {
-        "region": "us-east-1",
-        "access_key": "YOUR_TEST_ACCESS_KEY",
-        "secret_key": "YOUR_TEST_SECRET_KEY"
-    },
-
-    "MO": {
-        "region": "us-east-1",
-        "access_key": "YOUR_MO_ACCESS_KEY",
-        "secret_key": "YOUR_MO_SECRET_KEY"
-    },
-
-    "PROD": {
-        "region": "us-east-1",
-        "access_key": "YOUR_PROD_ACCESS_KEY",
-        "secret_key": "YOUR_PROD_SECRET_KEY"
-    }
-}
-
-
 TABLES = {
 
-    "DEV": [
-        "pabio-bill-lookup-table-dev"
-    ],
+    "DEV": "pablo-bill-lookup-table-dev",
 
-    "TEST": [
-        "pabio-bill-lookup-table-test"
-    ],
+    "TEST": "pablo-bill-lookup-table-test",
 
-    "MO": [
-        "pabio-bill-lookup-table-mo"
-    ],
+    "MO": "pablo-bill-lookup-table-mo",
 
-    "PROD": [
-        "pabio-bill-lookup-table-prod"
-    ]
+    "PROD": "pablo-bill-lookup-table-prod"
 }
