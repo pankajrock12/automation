c1d44dc3c045a6117123a6826eccc0f72d0ce3b8
pankajrock12 <pankajpandey.rock12@gmail.com>
2026-05-19 17:04:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,41 @@
+AWS_CONFIGS = {
+
+    "DEV": {
+        "aws_access_key_id": "NEW_DEV_ACCESS_KEY",
+        "aws_secret_access_key": "NEW_DEV_SECRET_KEY",
+        "aws_session_token": "NEW_DEV_SESSION_TOKEN",
+        "region": "us-east-1"
+    },
+
+    "TEST": {
+        "aws_access_key_id": "NEW_TEST_ACCESS_KEY",
+        "aws_secret_access_key": "NEW_TEST_SECRET_KEY",
+        "aws_session_token": "NEW_TEST_SESSION_TOKEN",
+        "region": "us-east-1"
+    },
+
+    "MO": {
+        "aws_access_key_id": "NEW_MO_ACCESS_KEY",
+        "aws_secret_access_key": "NEW_MO_SECRET_KEY",
+        "aws_session_token": "NEW_MO_SESSION_TOKEN",
+        "region": "us-east-1"
+    },
+
+    "PROD": {
+        "aws_access_key_id": "NEW_PROD_ACCESS_KEY",
+        "aws_secret_access_key": "NEW_PROD_SECRET_KEY",
+        "aws_session_token": "NEW_PROD_SESSION_TOKEN",
+        "region": "us-east-1"
+    }
+
+}
+
+
 TABLES = {
 
     "DEV": "pablo-bill-lookup-table-dev",
-
     "TEST": "pablo-bill-lookup-table-test",
-
     "MO": "pablo-bill-lookup-table-mo",
-
     "PROD": "pablo-bill-lookup-table-prod"
+
 }
